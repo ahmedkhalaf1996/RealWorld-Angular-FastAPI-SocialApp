27de07c02d13f851020da3f6dfffb05019b72543
ahmedkhalaf7396 <ahmedkhalaf7396@gmail.com>
2024-04-25 08:23:46 +0200

    api created

---
+++ backend/api/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Welcome To Soical"}
+
+if __name__== "__main__":
+    
+    uvicorn.run(app, host='0.0.0.0', port=5000)
+
